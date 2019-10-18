ce683d385fbcd567ac29591b9c44ecfca3ce647d
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-18 21:12:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,13 +10,9 @@ def directors_totals(nds)
   result = {}
 director_index = 0
 while director_index < nds.length do
-  director = nds[director_index][:name]
+  director = nds[director_index]
   movie_index = 0
-  result[director] = 0
-  while movie_index < nds[director_index][:movies].length do
-    result[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
-    movie_index += 1
-  end
+  result[director][:name] = gross_for_director(director_data)
   director_index += 1
 end
 result
@@ -33,15 +29,4 @@ def gross_for_director(director_data)
     movie_index += 1
   end
   total
-end
-
-def total_value_of_spinner(nds, row_index, column_index)
-  coordinate_total = 0
-  inner_len = nds[row_index][column_index].length
-  inner_index = 0
-  while inner_index < inner_len do
-    coordinate_total += nds[row_index][column_index][inner_index][:price]
-    inner_index += 1
-  end
-  coordinate_total
 end
