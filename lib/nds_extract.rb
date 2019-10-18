ca12ee4651d4f1afa15fee6fa2798b6b9c9c0696
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-18 21:00:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,6 +25,20 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  total_hash = directors_totals(directors_database)
-  pp total_hash[:director_data]
+  total = 0
+  while movie_index < nds[director_data][:movies].length do
+    total += nds[director_data][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+end  
+
+def total_value_of_spinner(nds, row_index, column_index)
+  coordinate_total = 0
+  inner_len = nds[row_index][column_index].length
+  inner_index = 0
+  while inner_index < inner_len do
+    coordinate_total += nds[row_index][column_index][inner_index][:price]
+    inner_index += 1
+  end
+  coordinate_total
 end
