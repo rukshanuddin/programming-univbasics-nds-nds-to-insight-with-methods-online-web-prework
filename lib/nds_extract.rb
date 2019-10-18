12e1e8fb0eacfceaae76b822b34920645f279016
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-18 21:14:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ result = {}
 director_index = 0
 while director_index < nds.length do
   director = nds[director_index]
-  result[director][:name] = gross_for_director(director)
+  result[director[:name]] = gross_for_director(director)
   director_index += 1
 end
 result
